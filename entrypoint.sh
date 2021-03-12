a12ed70407ce928615de427df26a7f79449f7a73
David James <djames@spreedly.com>
2021-03-12 16:50:51 -0500

    Debugging action
    Figured out why I was getting the following error:
    
    fatal: no path specified; see 'git help pull' for valid url syntax

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,19 +1,14 @@
-#!/bin/sh
-
-set -x
-
+#!/usr/bin/env sh
 
 GIT_USERNAME="${INPUT_USERNAME}"
 GIT_TOKEN="${INPUT_PASSWORD}"
 GIT_REPO="${INPUT_REPO}"
-GIT_BRANCH="${INPUT_BRANCH:-HEAD}"
+GIT_BRANCH="${INPUT_BRANCH}"
 
-# Setup to be able to pull roles with ansible-galaxy
 echo "https://$GIT_USERNAME:$GIT_TOKEN@github.com" > "${HOME}/.git-credentials"
 chmod 0600 "${HOME}/.git-credentials"
 git config --global credential.helper store
 
-
-SHA=$(git ls-remote "${GIT_REPO}" "${GIT_BRANCH}" | awk '{print $1}')
+SHA=`git ls-remote "${GIT_REPO}" "${GIT_BRANCH}" | awk '{print $1}'`
 
 echo "::set-output name=sha::$SHA"
