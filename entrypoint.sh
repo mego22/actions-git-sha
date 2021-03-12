8ec58e511bf75d6be11807a52dcdcec5638a21aa
David James <djames@spreedly.com>
2021-03-12 13:14:39 -0500

    Update variable name in entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,8 +3,8 @@
 
 GIT_USERNAME="${INPUT_USERNAME}"
 GIT_TOKEN="${INPUT_PASSWORD}"
-GIT_REPO="${INPUT_REPOSITORY}"
-GIT_BRANCH="${INPUT_REF:-HEAD}"
+GIT_REPO="${INPUT_REPO}"
+GIT_BRANCH="${INPUT_BRANCH:-HEAD}"
 
 # Setup to be able to pull roles with ansible-galaxy
 echo "https://$GIT_USERNAME:$GIT_TOKEN@github.com" > ~/.git-credentials
